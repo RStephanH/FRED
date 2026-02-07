15592403069112fa20bcce8244331e9798ded974
rsh <randrianirinahernandezstephan@gmail.com>
2026-02-07 22:12:06 +0300

    feat: command interface

--- core/domain/call/commands.go
+++ core/domain/call/commands.go
@@ -1,18 +1,30 @@
 package call
 
+type Command interface {
+	isCommand()
+}
+
 type AnswerCall struct {
 	CallID string
 }
+
+func (c AnswerCall) isCommand() {}
 
 type Playsound struct {
 	CallID string
 	File   string
 }
 
+func (c Playsound) isCommand() {}
+
 type StartRecording struct {
 	CallID string
 	Format string
 }
 
+func (c StartRecording) isCommand() {}
+
 type StopPlayback struct {
 }
+
+func (c StopPlayback) isCommand() {}
